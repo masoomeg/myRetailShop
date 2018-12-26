3faa19059be6209002cf697a31e9dcae1ec78e9e
masoomeg <masoome.gorji@gmail.com>
2018-12-26 16:37:53 +0330

    refactor project folder structure
    1- add some extra exception handlenig on server side
    2- add readme images
    3- add readme file

---
+++ backend/src/main/java/retailshop/domain/Product.java
@@ -0,0 +1,63 @@
+package retailshop.domain;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Table(name = "product")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+    private String description;
+    private String category;
+    private String amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+
+}
